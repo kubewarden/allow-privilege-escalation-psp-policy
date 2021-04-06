4ef88b30eac89814438f49b010ec52a4068bc633
Flavio Castelli <fcastelli@suse.com>
2021-04-06 21:58:16 +0200

    Implement validate_settings callback
    This is requested to load policies

--- src/settings.rs
+++ src/settings.rs
@@ -2,3 +2,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub(crate) struct Settings {}
+
+impl kubewarden_policy_sdk::settings::Validatable for Settings {
+    fn validate(&self) -> Result<(), String> {
+        Ok(())
+    }
+}
