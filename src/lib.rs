ea583a23df85a40ee0005260b0b7c85635986fb2
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-02 16:35:35 +0200

    Register `protocol_version`

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ mod settings;
 use settings::Settings;
 
 use kubewarden_policy_sdk::{
-    accept_request, reject_request, request::ValidationRequest, validate_settings,
+    accept_request, protocol_version_guest, reject_request, request::ValidationRequest,
+    validate_settings,
 };
 
 use k8s_openapi::api::core::v1 as apicore;
@@ -14,6 +15,7 @@ use k8s_openapi::api::core::v1 as apicore;
 pub extern "C" fn wapc_init() {
     register_function("validate", validate);
     register_function("validate_settings", validate_settings::<Settings>);
+    register_function("protocol_version", protocol_version_guest);
 }
 
 fn validate(payload: &[u8]) -> CallResult {
