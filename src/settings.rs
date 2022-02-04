65a66a504e84881049fa33be30b2fcf20d81c0eb
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-02-04 15:56:34 -0300

    Allow empty settings.
    The policy should allow the user leave the settings field empty. This
    was not working because serde_yaml tries to find all the fields unless
    we use the #[serde(default)]. This commit add this attribute to fix
    the issue and tests to ensure it's working as expected.

--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(default)]
 pub(crate) struct Settings {
     pub default_allow_privilege_escalation: bool,
 }
@@ -15,6 +16,39 @@ impl Default for Settings {
 
 impl kubewarden_policy_sdk::settings::Validatable for Settings {
     fn validate(&self) -> Result<(), String> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_yaml;
+
+    #[test]
+    fn test_policy_with_no_settings() -> Result<(), ()> {
+        let payload = "settings:";
+        let settings = serde_yaml::from_str::<Settings>(payload);
+        assert!(
+            settings.is_ok(),
+            "settings parse should not fails if it is empty"
+        );
+        assert!(
+            settings.unwrap().default_allow_privilege_escalation,
+            "default_allow_privilege_escalation should be 'true' when not defined by the user"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_policy_with_settings() -> Result<(), serde_yaml::Error> {
+        let payload = "default_allow_privilege_escalation: true";
+        let settings = serde_yaml::from_str::<Settings>(payload)?;
+        assert!(settings.default_allow_privilege_escalation);
+
+        let payload = "default_allow_privilege_escalation: false";
+        let settings = serde_yaml::from_str::<Settings>(payload)?;
+        assert_eq!(settings.default_allow_privilege_escalation, false);
         Ok(())
     }
 }
